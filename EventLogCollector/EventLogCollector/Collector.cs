b9d2f96ca794bb3b16190714e0ac8ac2d3fc83a2
Hyun Park <hyunpark@endcs.net>
2022-06-21 17:42:51 +0900

    지정한 쿼리 옵션에 따라 이벤트 로그를 추출할 수 있는 라이브러리 만들어진 라이브러리를 테스트 하기 위한 Console 프로그램

---
+++ EventLogCollector/EventLogCollector/Collector.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace EventLogCollector
+{
+    public class Collector
+    {
+        [Flags]
+        internal enum EventExportLogFlags
+        {
+            ChannelPath = 1,
+            LogFilePath = 2,
+            TolerateQueryErrors = 0x1000
+        };
+
+        [DllImport(@"wevtapi.dll",
+            CallingConvention = CallingConvention.Winapi,
+            CharSet = CharSet.Auto,
+            SetLastError = true)]
+        internal static extern bool EvtExportLog(
+            IntPtr sessionHandle,
+            string path,
+            string query,
+            string targetPath,
+            [MarshalAs(UnmanagedType.I4)] EventExportLogFlags flags);
+
+        public static bool Collect(string logSource, string query, string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            return EvtExportLog(IntPtr.Zero, logSource, query, path, EventExportLogFlags.ChannelPath);
+        }
+
+    }
+}
---
+++ EventLogCollector/EventLogCollectorConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EventLogCollector;
+var date =DateTime.Now.Subtract(TimeSpan.FromDays(3));
+var dateFormat = date.ToString("yyyy-MM-ddTHH:mm:ss");
+var query = $"*[System[(Level>=2 and Level<=3) and TimeCreated/@SystemTime >= '{dateFormat}']]";
+if(Collector.Collect("Application", query, "EventLog.evtx"))
+{
+    Console.WriteLine("Success");
+}
